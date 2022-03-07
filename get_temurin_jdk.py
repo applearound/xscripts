800b77ceaea6fbdff8c6dcc5553c265610fec45a
yezhou <yezhou@local.local>
2022-03-07 22:05:25 +0800

    完善下载jdk

--- get_temurin_jdk.py
+++ get_temurin_jdk.py
@@ -1,33 +1,57 @@
 import argparse
 import json
+import platform
 import ssl
-import urllib
 
-from http import client
+import http.client
+import urllib.parse
+import urllib.request
 
-HOST = "https://api.adoptium.net/v3/assets/latest/11/hotspot?release=latest&jvm_impl=hotspot&vendor=adoptium"
 
-query_params = {
-    "release": "latest",
-    "jvm_impl": "hotspot",
-    "vendor": "adoptium",
+def get_jdk_list(
+    jdk_version: str,
+    release: str = "release",
+    jvm_impl: str = "jvm_impl",
+    vendor: str = "vendor"
+) -> list[dict]:
+    connection = http.client.HTTPSConnection(
+        "api.adoptium.net", context=ssl._create_unverified_context())
+
+    connection.request(
+        method="GET",
+        url="/v3/assets/latest/{}/hotspot?{}".format(jdk_version, urllib.parse.urlencode({"release": "latest", "jvm_impl": "hotspot", "vendor": "adoptium"}))
+    )
+    response = connection.getresponse()
+
+    data = json.loads(response.read())
+
+    connection.close()
+
+    return data
+
+
+os_mapping = {
+    "Linux": "linux",
+    "Darwin": "mac",
+    "Windows": "windows"
 }
 
-p = urllib.parse.urlencode(query_params)
-
-connection = client.HTTPSConnection("api.adoptium.net", context=ssl._create_unverified_context())
-connection.request("GET", "/v3/assets/latest/11/hotspot?release=latest&jvm_impl=hotspot&vendor=adoptium")
-response = connection.getresponse()
-
-body_data = response.read()
-
-json_body = json.loads(body_data)
-
-the_one = list(filter(lambda x: x["binary"]["os"] == "windows" and x["binary"]["architecture"] == "x64" and x["binary"]["image_type"] == "jdk", json_body))[0]
-
-print(the_one["binary"]["package"]["link"])
-
-connection.close()
-
 if __name__ == "__main__":
-    pass
+    system: str = platform.system()
+    
+    jdk_list: list[dict] = get_jdk_list("11")
+
+    jdk_select_one = list(filter(lambda e: e["binary"]["os"] == os_mapping[system] and e["binary"]["image_type"] == "jdk" and e["binary"]["architecture"] == "x64", jdk_list))
+
+    assert len(jdk_select_one) == 1
+
+    jdk = jdk_select_one[0]
+
+    download_link = jdk["binary"]["package"]["link"]
+
+    proxy = urllib.request.ProxyHandler({'https': 'http://localhost:1091'})
+    opener = urllib.request.build_opener(proxy)
+    urllib.request.install_opener(opener)
+
+    code, msg = urllib.request.urlretrieve(download_link, "jdk.zip")
+    print(code, msg)
